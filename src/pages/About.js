bde8e094711169285d9998e0d85b4c42e48bce1c
Antony Chiramel <antonychiramel07@gmail.com>
2020-12-10 23:30:46 +0530

    Initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        reprehenderit excepturi vitae incidunt possimus optio perspiciatis ut
+        fugiat reiciendis. Blanditiis reiciendis libero odio modi dolores non
+        obcaecati, facilis error assumenda perspiciatis enim possimus culpa,
+        deleniti eveniet! Voluptatum quidem magni veniam!
+      </p>
+    </section>
+  );
+};
+
+export default About;
